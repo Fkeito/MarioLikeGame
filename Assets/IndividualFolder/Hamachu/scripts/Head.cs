4b1c7e54e90d2c4170e5c840bceeea65854d218d
hamachu <hamachu@omatase.local>
2017-09-23 01:35:47 +0900

    スクリプト編集途中

---
+++ Assets/IndividualFolder/Hamachu/scripts/Head.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Head : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {   //Finishタグのオブジェクトは弱点
+        if (this.tag == "Finish" && other.tag == "Player")
+        {
+            Destroy(gameObject);
+            Debug.Log("Shell");
+        }
+    }
+}
