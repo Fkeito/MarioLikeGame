9014c9e314b57ce7ece85fbcc7883e7606d45487
LAPTOP-TQ95D2HU\小笠原　悠太 <1417024@ed.tus.ac.jp>
2017-09-23 13:33:00 +0900

    スクリプト編集 弾追加

--- Assets/IndividualFolder/Ogasawara/script/supermario.cs
+++ Assets/IndividualFolder/Ogasawara/script/supermario.cs
@@ -5,7 +5,7 @@ public class supermario : MonoBehaviour{
 
     void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.tag == "mushroom")
+        if (other.gameObject.tag == "item")
         {
             transform.localScale = new Vector3(2, 2, 2);
         }
